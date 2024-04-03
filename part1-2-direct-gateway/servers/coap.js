0edcbb9a7420e7ed46c8952a2c7786f5af24d8b2
Luca <s1094518@studenti.univpm.it>
2024-04-03 19:07:13 +0100

    fix

--- part1-2-direct-gateway/servers/coap.js
+++ part1-2-direct-gateway/servers/coap.js
@@ -1,37 +1,38 @@
-var coap = require('coap'),  //#A
-  utils = require('./../utils/utils');
+var coap = require('coap'),  // Require the Node.js CoAP module you installed
+
+utils = require('./../utils/utils');
 
 var port = 5683;
 
 coap.createServer(function (req, res) {
-  console.info('CoAP device got a request for %s', req.url);
-  if (req.headers['Accept'] != 'application/json') {
-    res.code = '4.06'; //#B
-    return res.end();
-  }
-  switch (req.url) { //#C
-    case "/co2":
-      respond(res, {'co2': utils.randomInt(0, 1000)}); //#D
-      break;
-    case "/temp":
-      respond(res, {'temp': utils.randomInt(0, 40)});
-      break;
-    default:
-      respond(res);
-  }
+console.info('CoAP device got a request for %s', req.url);
+if (req.headers['Accept'] != 'application/json') {
+  res.code = '4.06'; // You only serve JSON, so you reply with a 4.06 (= HTTP 406: Not acceptable)
+  return res.end();
+}
+switch (req.url) { // Handle the different resources requested
+  case "/co2":
+    respond(res, {'co2': utils.randomInt(0, 1000)}); // This is the CO2 resource; generate a random value for it and respond
+    break;
+  case "/temp":
+    respond(res, {'temp': utils.randomInt(0, 40)});
+    break;
+  default:
+    respond(res);
+}
 }).listen(port, function () {
-  console.log("CoAP server started on port %s", port)
-});//#E
+console.log("CoAP server started on port %s", port)
+});// Start the CoAP server on port 5683 (CoAP’s default port)
 
-function respond(res, content) { //#F
-  if (content) {
-    res.setOption('Content-Format', 'application/json');
-    res.code = '2.05';
-    res.end(JSON.stringify(content));
-  } else {
-    res.code = '4.04';
-    res.end();
-  }
+function respond(res, content) { // Send the JSON content back or reply with a 4.04 (= HTTP 404: Not found)
+if (content) {
+  res.setOption('Content-Format', 'application/json');
+  res.code = '2.05';
+  res.end(JSON.stringify(content));
+} else {
+  res.code = '4.04';
+  res.end();
+}
 };
 //#A Require the Node.js CoAP module you installed
 //#B You only serve JSON, so you reply with a 4.06 (= HTTP 406: Not acceptable)
--- part3-cloud/my_plug.js
+++ part3-cloud/my_plug.js
@@ -1,16 +1,15 @@
 var mqtt = require('mqtt');
-
-var config = require('./my_config.json'); // #A 
-var thngId=config.thngId; 
-var thngUrl='/thngs/'+thngId;
-var thngApiKey=config.thngApiKey;
-var interval;
-var simulation = true;
-
+var config = require('./my_config.json'); // Importing configuration from a JSON file
 var exec = require('child_process').exec;
 
-// callback function that waits for the command to be executed returns it as an string
-// datatype = [Temperature, Pressure, Relative humidity]
+var thngId=config.thngId; // Storing the thngId from the configuration
+var thngUrl='/thngs/'+thngId; // Creating the URL for the thng
+var thngApiKey=config.thngApiKey; // Storing the API key from the configuration
+var interval;
+var simulation = true; // Flag to determine if simulation mode is enabled
+
+
+// Callback function that waits for the command to be executed and returns it as a string
 function CaptureOutput(callback) {
     // weather1.py and http server must be in the same folder
     exec("python3 weather1.py", (error, data) => {
@@ -20,38 +19,38 @@ function CaptureOutput(callback) {
 
 console.log('Using Thng #'+thngId+' with API Key: '+ thngApiKey);
 
-var client = mqtt.connect("mqtts://mqtt.evrythng.com:8883", {// #B
+var client = mqtt.connect("mqtts://mqtt.evrythng.com:8883", { // Connecting to the MQTT broker
   username: 'authorization',
   password: thngApiKey 
 });
 
-client.on('connect', function () { // #C
-  client.subscribe(thngUrl+'/properties/'); //#D
-  updateProperty('livenow', true); //#E
+client.on('connect', function () { // Event handler for when the client is connected
+  client.subscribe(thngUrl+'/properties/'); // Subscribing to the properties topic
+  updateProperty('livenow', true); // Updating the 'livenow' property to true
 
-  if (!interval) interval = setInterval(updateProperties, 5000); //#F
+  if (!interval) interval = setInterval(updateProperties, 5000); // #F - Setting up a periodic update of properties
 });
 
-client.on('message', function (topic, message) { // #G
+client.on('message', function (topic, message) { // Event handler for when a message is received
     console.log(message.toString());
 });
 
 
 function updateProperties () {
     if(simulation){
-        var temperature = (219.5 + Math.random()).toFixed(3); // #H
-        updateProperty ('temperature',temperature);
+        var temperature = (219.5 + Math.random()).toFixed(3); // Generating a random temperature value
+        updateProperty ('temperature',temperature); // Updating the 'temperature' property
       
-        var pressure = (Math.random()*10).toFixed(3); // #I
-        updateProperty ('pressure',pressure);
+        var pressure = (Math.random()*10).toFixed(3); // Generating a random pressure value
+        updateProperty ('pressure',pressure); // Updating the 'pressure' property
       
-        var humidity = (0.6+Math.random()/10).toFixed(3); // #J
-        updateProperty ('humidity',humidity);
+        var humidity = (0.6+Math.random()/10).toFixed(3); //Generating a random humidity value
+        updateProperty ('humidity',humidity); // Updating the 'humidity' property
     }else{
-        CaptureOutput(function(sensor_data) { // #H
-            updateProperty ('temperature', sensor_data['Temperature'].toFixed(3));
-            updateProperty ('pressure', sensor_data['Pressure'].toFixed(3));
-            updateProperty ('humidity', sensor_data['Relative humidity'].toFixed(3));
+        CaptureOutput(function(sensor_data) { // Capturing sensor data from an external source
+            updateProperty ('temperature', sensor_data['Temperature'].toFixed(3)); // Updating the 'temperature' property
+            updateProperty ('pressure', sensor_data['Pressure'].toFixed(3)); // Updating the 'pressure' property
+            updateProperty ('humidity', sensor_data['Relative humidity'].toFixed(3)); // Updating the 'humidity' property
         });
 
     }
@@ -59,24 +58,13 @@ function updateProperties () {
 }
 
 function updateProperty (property,value) {
-    client.publish(thngUrl+'/properties/'+property, '[{"value": '+value+'}]');
+    client.publish(thngUrl+'/properties/'+property, '[{"value": '+value+'}]'); // Publishing the updated property value
 }
 
 // Let's close this connection cleanly
-process.on('SIGINT', function() { // #K
+process.on('SIGINT', function() { // Cleanly exit this code and set the livenow property to false
   clearInterval(interval);
-  updateProperty ('livenow', false);
-  client.end();
-  process.exit();
+  updateProperty ('livenow', false); // Updating the 'livenow' property to false
+  client.end(); // Closing the MQTT connection
+  process.exit(); // Exiting the process
 });
-//#A Load configuration from file (Thng ID and Thng API key)
-//#B Connect to the secure MQTT server on EVRYTHNG
-//#C Callback called once when the MQTT connection suceeds
-//#D Subscribe to all properties
-//#E Set the property livenow to true
-//#F Call the function updateProperties() in 5 seconds
-//#G Called every time an MQTT message is received from the broker
-//#H Measures voltage (fluctuates around ~220 volts)
-//#I Measures current (fluctuates 0–10 amps)
-//#J Measures power using P=U*I*PF (PF=power factor fluctuates 60–70%)
-//#K Cleanly exit this code and set the livenow property to false
